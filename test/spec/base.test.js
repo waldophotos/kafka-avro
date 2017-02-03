55c06f201633cff4f828eefe2854f1220c3ccc50
Thanasis Polychronakis <thanpolas@gmail.com>
2017-02-03 16:40:08 +0200

    expand on base tests

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -3,11 +3,44 @@
  */
 const chai = require('chai');
 const expect = chai.expect;
+const sinon = require('sinon');
 
 const KafkaAvro = require('../..');
 
+const testLib = require('../lib/test.lib');
+
 describe('Base API Surface', function() {
+  testLib.init();
+
   it('should expose expected methods', function(){
     expect(KafkaAvro).to.be.a('function');
+  });
+
+  describe('Edge cases', function() {
+    beforeEach(function() {
+      this.initSpy = sinon.spy(KafkaAvro.prototype, 'init');
+    });
+    afterEach(function() {
+      this.initSpy.restore();
+    });
+    it('Should instantiate multiple instances', function() {
+      let kafkaAvro = new KafkaAvro({
+        kafkaBroker: testLib.KAFKA_BROKER_URL,
+        schemaRegistry: testLib.KAFKA_SCHEMA_REGISTRY_URL,
+      });
+
+      return kafkaAvro.init()
+        .bind(this)
+        .then(function() {
+          kafkaAvro = new KafkaAvro({
+            kafkaBroker: testLib.KAFKA_BROKER_URL,
+            schemaRegistry: testLib.KAFKA_SCHEMA_REGISTRY_URL,
+          });
+          return kafkaAvro.init();
+        })
+        .then(function() {
+          expect(this.initSpy.callCount).to.equal(2);
+        });
+    });
   });
 });
